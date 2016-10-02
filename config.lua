0406c3422466fe3526101b5ce9d0258380bcd360
Marcel H <Migno@users.noreply.github.com>
2016-10-02 07:10:19 +0200

    Pathfinder Update

--- config.lua
+++ config.lua
@@ -1,36 +1,11 @@
--- ##########################################################
--- #              -- CONFIGURATION FILE --                  #
--- #      Please edit this config to match your needs       #
--- #              Copyright © 2016 imMigno                  #
--- ##########################################################
-
--- The Bot will heal your Pokemon when the Count of usable Pokemon get to this number
 keepAlive = 5
 
--- The Bot will stop when your whole team achieve that level.
-StopLevel = 100
+MaxLevel = 100
 
--- Once the pokemon hit that level, it will stop switching and fighting by himself.
-SwapCap = 70
+LevelLocation = "Route 2"
 
--- Map specific configurations
--- Please insert the Mapnames of your Leveling routine
+LevelSpot = "Grass"
 
-Mount = "Bicycle"
-
-Pokecenter = ""
-City = ""
-Map1 = ""
-Map2 = ""
-Map3 = ""
-Map4 = ""
-Map5 = ""
-LevelLocation = ""
-
--- Please choose the method of moving on your Level Location : "Grass", "Water" or "Rectangle".
-LevelSpot = ""
-
--- Modify below only if you're using Rectangle as movement method.
 minX = 3
 minY = 15
 maxX = 7
